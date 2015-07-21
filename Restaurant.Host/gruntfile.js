6eb3efcb861db0ddc0fe909f752d38a3d11f7e11
Michelle Darlea <mdarlea@gmail.com>
2015-07-21 08:54:37 -0400

    Added ngdocs

---
+++ Restaurant.Host/gruntfile.js
@@ -0,0 +1,34 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        dist: 'dist',
+        pkg: grunt.file.readJSON('package.json'),
+        
+        ngdocs: {
+            options: {
+                dest: '<%= dist %>/docs',
+                title: '<%= pkg.name %>',
+                html5Mode: false,
+                sourceLink: 'https://github.com/mdarlea/Demo_Restaurant/blob/master/{{file}}'
+            },
+            api: {
+                src: ['app/**/*.js', '!app/**/*.spec.js'],
+                title: 'API Documentation'
+            }
+        },
+        connect: {
+            options: {
+                keepalive: true
+            },
+            server: {}
+        },
+        clean: ['<%= dist %>']
+    });
+    
+    grunt.loadNpmTasks('grunt-ngdocs');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    
+    grunt.registerTask('test', ['clean', 'ngdocs']);
+
+    grunt.registerTask('default', ['test', 'connect']);
+};
